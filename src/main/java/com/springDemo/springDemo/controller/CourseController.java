05998aacd325cc5bbb93ab8547a7163c703d0bc6
Zaheer Shaikh <73430101+zaheershaikh936@users.noreply.github.com>
2023-06-23 13:42:47 +0400

    added course

--- src/main/java/com/springDemo/springDemo/controller/CourseController.java
+++ src/main/java/com/springDemo/springDemo/controller/CourseController.java
@@ -1,11 +1,32 @@
 package com.springDemo.springDemo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.springDemo.springDemo.model.Course;
+import com.springDemo.springDemo.services.CourseServices;
+
 @RestController
+@RequestMapping("/course")
 public class CourseController {
 
-	public String test() {
-		return "Hello from Course";
+	@Autowired
+	private CourseServices courseServices;
+	
+	@PostMapping()
+	public Course create(@RequestBody Course courseData) {
+		return courseServices.create(courseData);
 	}
+	
+	@GetMapping()
+	public List<Course> findAll(){
+		return courseServices.findAll();
+	}
+	
 }
--- src/main/java/com/springDemo/springDemo/services/CourseServices.java
+++ src/main/java/com/springDemo/springDemo/services/CourseServices.java
@@ -1,15 +1,25 @@
 package com.springDemo.springDemo.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.springDemo.springDemo.model.Course;
 import com.springDemo.springDemo.repository.CourseRepository;
 
+@Service
 public class CourseServices implements ICourse {
 	@Autowired
 	private CourseRepository courseRepository;
 	
+	@Override
 	public Course create(Course courseBody) {
-		return courseRepository.save(courseBody)
+		return courseRepository.save(courseBody);
+	}
+	
+	@Override
+	public List<Course> findAll(){
+		return courseRepository.findAll();
 	}
 }
